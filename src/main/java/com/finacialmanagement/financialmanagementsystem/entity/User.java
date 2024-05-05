c78cd593fc6928d3e79a1b33390da6a0bb6da595
Thisara <sasmithajayasinghe1@gmail.com>
2024-05-05 13:31:06 +0530

    user

--- src/main/java/com/finacialmanagement/financialmanagementsystem/entity/User.java
+++ src/main/java/com/finacialmanagement/financialmanagementsystem/entity/User.java
@@ -55,4 +55,14 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
