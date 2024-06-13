d5a1c45025127bc067fc522c898345486fe583de
Thisara <sasmithajayasinghe1@gmail.com>
2024-06-13 22:44:42 +0530

    expense retrive

--- src/main/java/com/finacialmanagement/financialmanagementsystem/repository/ExpenseRepository.java
+++ src/main/java/com/finacialmanagement/financialmanagementsystem/repository/ExpenseRepository.java
@@ -1,4 +1,5 @@
 package com.finacialmanagement.financialmanagementsystem.repository;
+
 import com.finacialmanagement.financialmanagementsystem.entity.Expenses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
