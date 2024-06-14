d3886782a0d437915c7357d616c9def721b13c6d
Thisara <sasmithajayasinghe1@gmail.com>
2024-06-15 00:44:24 +0530

    transaction

--- src/main/java/com/finacialmanagement/financialmanagementsystem/controller/TransactionController.java
+++ src/main/java/com/finacialmanagement/financialmanagementsystem/controller/TransactionController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@CrossOrigin("http://localhost:5173")
-@RequestMapping("users/{userId}/incomes")
-public class TransactionController {
-}
+//@RestController
+//@CrossOrigin("http://localhost:5173")
+//@RequestMapping("users/{userId}/incomes")
+//public class TransactionController {
+//}
